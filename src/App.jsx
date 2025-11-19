5d167fc83598373574be6ea6a647ec587cb4e8f4
CamilleJones44 <danhumprey123@gmail.com>
2025-11-19 18:41:33 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,34 @@
-import { useReducer } from 'react';
+const items = ['Apples', 'Bananas', 'Cherries'];
 
-const initialState = {
-  loading: false,
-  error: null,
-  fact: null,
-};
+function Header({ title }) {
 
-function factReducer(state, action) {
-  switch (action.type) {
-    case 'FETCH_START':
-      return { loading: true, error: null, fact: null };
-    case 'FETCH_SUCCESS':
-      return { loading: false, error: null, fact: action.payload };
-    case 'FETCH_ERROR':
-      return { loading: false, error: action.payload, fact: null };
-    default:
-      return state;
-  }
+  
+  return <h1>{title}</h1>;
+}
+
+
+
+function List({ items }) {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
 }
 
 function App() {
-  const [state, dispatch] = useReducer(factReducer, initialState);
 
-  const getFact = () => {
-    dispatch({ type: 'FETCH_START' });
-
-    fetch("https://catfact.ninja/fact")
-      .then(res => res.json())
-      .then(data => dispatch({ type: 'FETCH_SUCCESS', payload: data.fact }))
-      .catch(err =>
-        dispatch({ type: 'FETCH_ERROR', payload: err.message })
-      );
-  };
-
+  
   return (
     <div>
-      <h1>Cat Fact Generator</h1>
-      <button onClick={getFact}>Get Cat Fact</button>
+      <Header title="My Grocery List" />
 
-      {state.loading && <p>Loading...</p>}
-      {state.fact && <p>{state.fact}</p>}
-      {state.error && <p style={{ color: "red" }}>{state.error}</p>}
+        <List items={items} />
+
+     
+      <List items={items} />
     </div>
   );
 }
